adcebc6d26213fc1714f529b19db7f883e97b4d9
Zarief <zzarieff@Zariefs-MBP.local>
2025-09-24 18:45:01 +0800

    added main menu and payment menu

--- program.cpp
+++ program.cpp
@@ -8,7 +8,7 @@ struct Transaction {
     string type;   //type of payment
     string store;
     double amount;
-    string transactionid;
+    string TransactionId;
 
 };
 
@@ -19,7 +19,65 @@ struct User {
     vector<Transaction> history;
 };
 
+User currentUser;
+int nextTransactionId = 1000;
 
-int main(){
+///function prototypes///
+void menuDisplay();
+void makePayment();
+
+
+void menuDisplay(){
+    cout << "--------------------------------" << endl;
+    cout << "           E-Wallet             " << endl;
+    //add more here
+}
+
+
+
+
+void makePayment(){
+    cout << "===== CHOOSE STORE =====" << endl;
+    cout << " 1. McDonald's " << endl;
+    cout << " 2. KFC " << endl;
+    cout << " 3. Grab " << endl;
+    cout << " 4. FamilyMart" << endl;
+    cout << " 5. Shopee " << endl;
+    cout << " 6. Others " << endl;
+
+    int number;
+    cout << "Select store: ";
+    cin >> number;
+
+    string store;
+    switch (number)
+    {
+    case 1:
+        store = "McDonald's";
+        break;
+    case 2:
+        store = "KFC";
+        break;
+    case 3:
+        store = "Grab";
+        break;
+    case 4:
+        store = "FamilyMart";
+        break;
+    case 5:
+        store = "Shopee";
+        break;
+    case 6:
+        cout << "Enter store name: ";
+        cin.ignore();
+        getline(cin, store);
+        break;
+
+    default:
+        cout << "Invalid choice. Using 'Unknown store'" << endl;
+        store = "Unknown store";
+    }
+}
+int main() {
 
 }
